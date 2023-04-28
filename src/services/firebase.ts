1aa2ae4754acf0f13be07ccd2bb1d6f8b95828cd
João Santos <jvsantossud@gmail.com>
2023-04-28 14:31:01 -0300

    application completed

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+import Firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+Firebase.initializeApp(firebaseConfig);
+
+const auth = Firebase.auth();
+const database = Firebase.database();
+
+export { Firebase, auth, database }
